ef16bd0a999da0340b63973dec6d6d7d3ce88382
malsabi <xxdarklord65xx@gmail.com>
2023-10-23 11:27:45 +0400

    Add project files.

---
+++ WindowsFlyoutItemIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsFlyoutItemIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
